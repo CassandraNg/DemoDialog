3a86bfe14cb4bac874abee445b15b9423aa2be6e
CassandraNg <20017476@myrp.edu.sg>
2021-08-04 12:17:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20017476/demodialog/MainActivity.java
@@ -0,0 +1,227 @@
+package sg.edu.rp.c346.id20017476.demodialog;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnDemo1, btnDemo2, btnDemo3, btnEx3, btnDemo4, btnDemo5;
+    TextView tvDemo2, tvDemo3, tvEx3, tvDemo4, tvDemo5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDemo1 = findViewById(R.id.buttonDemo1);
+        btnDemo2 = findViewById(R.id.buttonDemo2);
+        tvDemo2 = findViewById(R.id.textViewDemo2);
+        btnDemo3 = findViewById(R.id.buttonDemo3);
+        tvDemo3 = findViewById(R.id.textViewDemo3);
+        btnEx3 = findViewById(R.id.Exercise3);
+        tvEx3 = findViewById(R.id.textViewEx3);
+        btnDemo4 = findViewById(R.id.buttonDemo4);
+        tvDemo4 = findViewById(R.id.textViewDemo4);
+        btnDemo5 = findViewById(R.id.buttonDemo5);
+        tvDemo5 = findViewById(R.id.textViewDemo5);
+
+        btnDemo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create the Dialog builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                // Set the dialog details
+                /*
+                myBuilder.setTitle("Demo 1 - Simple Dialog");
+                myBuilder.setMessage("I can develop Android App.");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Close",null);
+                */
+
+                myBuilder.setTitle("Congratulations");
+                myBuilder.setMessage("You have completed a simple Dialog Box");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Dismiss",null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+        btnDemo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                myBuilder.setTitle("Demo 2 Buttons Dialog");
+                myBuilder.setMessage("Select one of the Buttons below");
+                myBuilder.setCancelable(false);
+
+                //Configure the positive button
+                myBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvDemo2.setText("You have selected Positive");
+                    }
+                });
+
+                //Configure the neutral button
+                myBuilder.setNeutralButton("Cancel",null);
+
+
+                //Configure the negative button
+                myBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvDemo2.setText("You have selected Negative");
+                    }
+                });
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+
+            }
+        });
+
+        btnDemo3.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                // Inflate the input.xml layout file
+                LayoutInflater inflater =
+                        (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View viewDialog = inflater.inflate(R.layout.input, null);
+
+                // Obtain the UI component in the input.xml layout
+                // It needs to be defined as "final", so that it can used in the onClick() method later
+                final EditText etInput = viewDialog.findViewById(R.id.editTextInput);
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+                myBuilder.setView(viewDialog);  // Set the view of the dialog
+                myBuilder.setTitle("Demo 3-Text Input Dialog");
+                myBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Extract the text entered by the user
+                        String message = etInput.getText().toString();
+                        // Set the text to the TextView
+                        tvDemo3.setText(message);
+                    }
+                });
+                myBuilder.setNegativeButton("CANCEL", null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+        btnEx3.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                // Inflate the input.xml layout file
+                LayoutInflater inflater =
+                        (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View viewDialog = inflater.inflate(R.layout.sum, null);
+
+                // Obtain the UI component in the input.xml layout
+                // It needs to be defined as "final", so that it can used in the onClick() method later
+                final EditText num1 = viewDialog.findViewById(R.id.editTextNum1);
+                final EditText num2 = viewDialog.findViewById(R.id.editTextNum2);
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+                myBuilder.setView(viewDialog);  // Set the view of the dialog
+                myBuilder.setTitle("Exercise 3");
+                myBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Extract the text entered by the user
+                        Integer add1 = Integer.valueOf(num1.getText().toString());
+                        Integer add2 = Integer.valueOf(num2.getText().toString());
+                        Integer total = add1 + add2;
+                        // Set the text to the TextView
+                        tvEx3.setText("The sum is "+total);
+                    }
+                });
+                myBuilder.setNegativeButton("CANCEL", null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+        btnDemo4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create the listener to set the date
+                DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        tvDemo4.setText("Date: " + dayOfMonth + "/" + (monthOfYear+1) + "/"+ year);
+                    }
+                };
+
+                //create the date picker dialog
+                /*DatePickerDialog myDateDialog = new DatePickerDialog(MainActivity.this, myDateListener, 2014,11,31);
+                myDateDialog.show();
+                 */
+
+                //create the date picker dialog to show the current date when it first appear
+                Calendar now = Calendar.getInstance();
+                int year = now.get(Calendar.YEAR);
+                int month = now.get(Calendar.MONTH);
+                int day = now.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog myDateDialog = new DatePickerDialog(MainActivity.this,
+                        myDateListener, year, month, day);
+
+                myDateDialog.show();
+
+
+            }
+        });
+
+        btnDemo5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create listener to set time
+                TimePickerDialog.OnTimeSetListener myTimeListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        tvDemo5.setText("Time "+hourOfDay+":"+ new DecimalFormat("00").format(minute));
+                    }
+                };
+
+                //Create time picker dialog
+                //TimePickerDialog myTimeDialog = new TimePickerDialog(MainActivity.this,myTimeListener,20,00,false);
+
+                // Create the Date Picker Dialog to show the current date when it first appears
+                Calendar now = Calendar.getInstance();
+                int hourOfDay = now.get(Calendar.HOUR_OF_DAY);  // Current Hour
+                int minute = now.get(Calendar.MINUTE);  // Current Minute
+                TimePickerDialog myTimeDialog = new TimePickerDialog(MainActivity.this,
+                        myTimeListener, hourOfDay, minute, true);
+
+                myTimeDialog.show();
+            }
+        });
+
+    }
+}
